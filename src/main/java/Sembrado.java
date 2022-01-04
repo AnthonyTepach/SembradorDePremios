7580604d19383cc80725d146202a5fcfa2cc96e8
Anthony Tepach <anthony.tepach@gmail.com>
2022-01-04 14:35:22 -0600

    writeFile() ok

--- src/main/java/Sembrado.java
+++ src/main/java/Sembrado.java
@@ -1,6 +1,9 @@
 import files.FileBrowser;
 
+import javax.swing.*;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -10,7 +13,7 @@ public class Sembrado {
     ArrayList<String> allDataSeeding;
     Map<String, String> ext;
     Random r;
-    Sembrado() {
+    protected Sembrado() {
         allDataSeeding = new ArrayList<>();
         ext= new HashMap<>();
         ext.put("Desc", "Archivo de Texto");
@@ -19,13 +22,28 @@ public class Sembrado {
         r = new Random();
     }
 
-    public void writeFile(){
-
+    protected void writeFile() {
+        try {
+            FileWriter fw = new FileWriter(new FileBrowser().saveAs().concat("/sembrado-sucursales-oblatos.txt"), Charset.forName("utf-8"));
+            fw.write("FOLIO\tFOLIO_X_SUCURSAL\tUUID\tSUCURSAL\tPREMIO\tCORRECTA\tPREGUNTA\tR1\tR2\tR3\n");
+            for (int i = 0; i < this.allDataSeeding.size(); i++) {
+                fw.write(String.format("%05d",(i+1))+"\t"+this.allDataSeeding.get(i));
+                fw.write("\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            JOptionPane.showMessageDialog(null,"Finalizado");
+        }
     }
 
-    private void  readFile() {
+    protected void  readFile() {
         try {
+            JOptionPane.showMessageDialog(null,"Selecciona BD de Premios");
             List<String> listTxt = Files.readAllLines(Paths.get(new FileBrowser().openFile(ext)), StandardCharsets.UTF_8);
+            JOptionPane.showMessageDialog(null,"Selecciona BD de Preguntas");
+            List<String> questionsTxt = Files.readAllLines(Paths.get(new FileBrowser().openFile(ext)), StandardCharsets.UTF_8);
             for(String rowTxt :listTxt) {
                String columsRowTxt[]= rowTxt.split("\t");
                List<String> dataSeeding = new ArrayList<>();
@@ -39,7 +57,8 @@ public class Sembrado {
 
                 for(int i=0;i<Integer.parseInt(columsRowTxt[2]);i++){
                     int randomNumber = r.nextInt(dataSeeding.size());
-                    randomDataSeeding.add(String.format("%05d",(i+1))+"\t"+columsRowTxt[0]+"\t"+columsRowTxt[1]+"\t"+dataSeeding.get(randomNumber).substring(0,1));
+                    int randomQuestion = r.nextInt(questionsTxt.size());
+                    randomDataSeeding.add(String.format("%05d",(i+1))+"\t"+columsRowTxt[0]+"\t"+columsRowTxt[1]+"\t"+dataSeeding.get(randomNumber).substring(0,1)+"\t"+questionsTxt.get(randomQuestion));
                     dataSeeding.remove(randomNumber);
                 }
 
