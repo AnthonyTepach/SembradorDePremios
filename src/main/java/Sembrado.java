a31347d5a3552f32b2e0647fdf8c9d679c4c9327
Anthony Tepach <anthony.tepach@gmail.com>
2022-01-04 11:10:33 -0600

    Definicion de tipo de archivo que se puede abrir

--- src/main/java/Sembrado.java
+++ src/main/java/Sembrado.java
@@ -1,2 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Sembrado {
+    //Extencion de archivo
+    Map<String, String> ext;
+
+    Sembrado() {
+        ext= new HashMap<>();
+        ext.put("Desc", "Archivo de Texto");
+        ext.put("Ext", ".txt");
+        ext.put("Type", "TXT");
+    }
+
+    public void readFile(){
+
+    }
+
+    private void writeFile() {
+
+    }
 }
